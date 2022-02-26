a2d3478db7fd4c7edc0d216d7eefb3d986cd268b
Lucas Carnero <desarrollo@lucascarnero.com.ar>
2022-02-25 23:02:52 -0300

    agregar modulo de notas

---
+++ app/Models/Nota.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Nota extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'destination_user_id',
+        'nota_numerica',
+        'nota_letras'
+    ];
+}
